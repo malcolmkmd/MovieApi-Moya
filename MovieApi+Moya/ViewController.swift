8eb6a0e132e041bd8b5456cae6064293f029ea7b
Malcolm Kumwenda <malcolm.kumwenda@zapper.com>
2018-01-16 14:52:05 +0200

    Initial Commit

---
+++ MovieApi+Moya/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MovieApi+Moya
+//
+//  Created by Malcolm Kumwenda on 2018/01/16.
+//  Copyright © 2018 mdevsa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
